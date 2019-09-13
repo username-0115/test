6ab03a561f964b1733bf31650e43c493f564428b
username-0115 <a.o.m.c.i.o.m.c.m.r@gmail.com>
2019-09-13 17:55:43 +0900

    20190913

--- person2/src/person2/Test.java
+++ person2/src/person2/Test.java
@@ -1,29 +1,33 @@
 package person2;
 
 public class Test {
-	
+
 	public static void main(String[] args){
-		
-//		Person taro = new Person();
-//		taro.name="taro";
-//		taro.age=18;
-//		System.out.println(taro.name);
-//		System.out.println(taro.age);
-//		
-//		
-//		Person jiro = new Person("jiro",20);
-//		System.out.println(jiro.name);
-//		System.out.println(jiro.age);
-		
-		
-		Person jiro = new Person();
-		jiro.name="jiro";
-		jiro.age=18;
+
+		Person taro = new Person();
+		taro.name="taro";
+		taro.age=18;
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+
+
+		Person jiro = new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
-		
-		
-//		Person  = new Person(null)
+
+		Person saburo = new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+		Person nanashi = new Person(25);
+		System.out.println(nanashi.name);
+		System.out.println(nanashi.age);
+
+		Person hanako = new Person("hanako",17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+
+
 	}
 
 }
