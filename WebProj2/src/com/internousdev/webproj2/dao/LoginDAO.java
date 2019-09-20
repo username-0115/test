cb75432b6fa0de972cd4627e76fbe983e62d4f4e
a.o.m.c.i.o.m.c.m.r@gmail.com <a.o.m.c.i.o.m.c.m.r@gmail.com>
2019-09-20 17:56:21 +0900

    20190920

--- WebProj2/src/com/internousdev/webproj2/dao/LoginDAO.java
+++ WebProj2/src/com/internousdev/webproj2/dao/LoginDAO.java
@@ -17,7 +17,7 @@ public class LoginDAO {
 		DBConnector db=new DBConnector();
 		Connection con=db.getConnection();
 
-		String sql="select * from users where user_name-? and password=?";
+		String sql="select * from users where user_name=? and password=?";
 		try{
 			PreparedStatement ps=con.prepareStatement(sql);
 			ps.setString(1, username);
